df4867fa0bea364f86e3c1c0e22bfef9c15630af
本川　晴彦 <h.motokawa@salop.co.jp>
2024-04-29 13:38:12 +0900

    プロジェクト作成

---
+++ android/app/src/main/kotlin/com/example/sample_of_dividing_widget_into_manageable_parts/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sample_of_dividing_widget_into_manageable_parts
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: HomeScreen(),
+      ),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('no_refactor'),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('refactor'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
